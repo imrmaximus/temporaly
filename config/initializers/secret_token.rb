65fd91c477b30fb90510c0e8d0bb3d4eb141c076
imrmaximus <maxime_avon@icloud.com>
2014-04-27 08:39:32 +0200

    1er commit avec l'interface activeadmin!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiteApi::Application.config.secret_key_base = '1601e5b92fecb9814698f17c96fa4e5f9777e869a28e2122328ba18fcbbad4e34fa421c60d9c63bac0ee75af736b32f56bf8d82002b55669476a004811519de0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SiteApi::Application.config.session_store :cookie_store, key: '_Site_api_session'
